38334b44dfa9ac0c93e648005631cc1eb626c3b9
Jimmy Cuasquer <jimmioswaldojla@gmail.com>
2021-08-19 13:16:40 -0500

    actualizacion scroll.js

--- librerias/assets/scroll.js
+++ librerias/assets/scroll.js
@@ -5,46 +5,24 @@ const scrollButton2 = document.getElementById("scroll-button2");
 const crearportafolio = document.getElementById("what-create-porfolio")
 const scrollButton3 = document.getElementById("scroll-button3")
 const tipoportafolio = document.getElementById("type-porfolio")
-/*
-function scrolHeaderTop() {
-    console.log("scrolHeaderTop")
- if(window.scrollY > 100){
-    header.classList.add("fixed-header")
- }else if(window.scrollY === 0){
-     header.classList.remove("fixed-header")
- }
-}
-*/
+
 function scrolHeaderTop() {
      console.log("scrolHeaderTop")
  if(window.scrollY > 300){
-  //  console.log("entro");
     header.classList.add("fixed-header")
-  
  }else if(window.scrollY === 0){
     header.classList.remove("fixed-header")
-
 }
 }
-
 function scrollWindowTop() {
-    // let offsetMain = mainSection.offsetTop;
-   // let x =inicio.offsetHeight;
     let offsetinicio = inicio.offsetTop
-   
-    //let offsetcreatePorfolio = offsetinicio - scrollY; 
-     //console.log (window.scrollY)  
     window.scrollTo(0, offsetinicio);
-    
-
-    // scrollWindowTop
   }
   function scrollWindowButton2() {
     let offsetheight = crearportafolio.offsetHeight -400;
     let offsetcreatePorfolio = crearportafolio.offsetTop - offsetheight ;
       window.scrollTo(0, offsetcreatePorfolio);
   }
-
   function scrollWindowButton3() {
     let offsetheight = crearportafolio.offsetHeight -450;
     let typePorfolio = tipoportafolio.offsetTop - offsetheight;
@@ -54,7 +32,6 @@ function scrollWindowTop() {
   }
 
 document.addEventListener("scroll", scrolHeaderTop)
-//document.onscroll = scrolHeaderTop;
 scrollButton.onclick = scrollWindowTop;
 scrollButton2.onclick = scrollWindowButton2;
 scrollButton3.onclick = scrollWindowButton3
